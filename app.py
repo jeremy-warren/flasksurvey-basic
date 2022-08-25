8fccda6c23bf2db9fa5ac6a6b2bc1e6c03fd4000
Jeremy Warren <j3warren@gmail.com>
2022-08-25 17:26:12 -0400

    initial commit

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, flash
+from flask import Flask, render_template, request, session, redirect, flash
 from surveys import satisfaction_survey as survey
 # from flask_debugtoolbar import DebugToolbarExtension
 
@@ -14,16 +14,31 @@ responses = []
 
 
 @app.route('/')
-def start_survey():
-    return render_template("/instructions.html")
+def load_base():
+    return render_template("/instructions.html", survey=survey)
 
 
-@app.route('/questions/<int:qid>')
-def show_question():
-    return render_template("/question.html")
+@app.route('/start', methods=['POST', 'GET'])
+def load_first_question():
+
+    return redirect('question/0.html')
 
 
-@app.route('/answer', methods="POST")
+@app.route('/question/<int:qid>')
+def show_question(qid):
+    if qid == len(responses):
+        question = survey.questions[qid]
+        return render_template("/question.html/", question_num=qid, question=question)
+    else:
+        flash(f"No skipping around, please!")
+        return redirect(f"/question/{len(responses)}")
+
+
+@app.route('/answer', methods=["POST", "GET"])
 def next_question():
     choice = request.form['answer']
     responses.append(choice)
+    if len(responses) == len(survey.questions):
+        return render_template("thank_you.html")
+    elif len(responses) < len(survey.questions):
+        return redirect(f"question/{len(responses)}")
