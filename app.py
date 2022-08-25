2190b85f1762d7635098b3db782e775fc306ce1b
Jeremy Warren <j3warren@gmail.com>
2022-08-25 15:01:11 -0400

    try to remove venv

--- app.py
+++ app.py
@@ -1,13 +1,29 @@
-from flask import Flask, render_template
-from surveys import Question, Survey
+from flask import Flask, render_template, request, redirect, flash
+from surveys import satisfaction_survey as survey
 
 app = Flask(__name__)
-
-reponses = []
-
-
-@app('/')
-def rootpage()
+app.config['SECRET_KEY'] = ['dogs']
 
 
-render_template("base.html")
+responses = []
+
+# the base page shows the survey and a start button
+
+
+@app.route('/')
+def start_survey():
+
+    return redirect("/instructions.html")
+
+
+@app.route('/instructions', methods='POST')
+def instructions():
+    return render_template("instructions.html")
+    responses = []
+
+
+@app.route('/questions/<int:qid>', methods='POST')
+def questions():
+    return render_template('questions/qid.html')
+    if not responses[0]:
+        print('lakdslalsjd')
